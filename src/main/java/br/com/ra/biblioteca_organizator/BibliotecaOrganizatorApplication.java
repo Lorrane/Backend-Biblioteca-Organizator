9910d74a6901781f59e8f8152919724a6e69d5be
Lorrane Machado <lorranemachado21@gmail.com>
2022-05-26 11:07:16 -0300

    SecurityConfig

---
+++ src/main/java/br/com/ra/biblioteca_organizator/BibliotecaOrganizatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.ra.biblioteca_organizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaOrganizatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaOrganizatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ra/biblioteca_organizator/BibliotecaOrganizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ra.biblioteca_organizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaOrganizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
