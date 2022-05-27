129243dd8b3de1784b628312e41eab114cd05420
Lorrane Machado <lorranemachado21@gmail.com>
2022-05-27 17:34:12 -0300

    create finds

--- src/main/java/br/com/ra/biblioteca_organizator/repositories/BooksRepository.java
+++ src/main/java/br/com/ra/biblioteca_organizator/repositories/BooksRepository.java
@@ -6,4 +6,14 @@ import br.com.ra.biblioteca_organizator.entities.Books;
 
 public interface BooksRepository extends JpaRepository<Books, Integer> {
 
+    Books findByIsbn(Integer isbn);
+
+    Books findByTitle(String title);
+
+    Books findBySubtitle(String subtitle);
+
+    Books findByAuthor(String author);
+
+    Books findByPubliching(String publishing);
+
 }
